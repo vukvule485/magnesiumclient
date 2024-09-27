8d4fe70581424e3b7900bd2e8dc2e8cdef29b511
ItsGuestyYT <116463543+vukvule485@users.noreply.github.com>
2024-09-27 11:20:46 +0200

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+// Add interactivity for smooth scroll on navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
